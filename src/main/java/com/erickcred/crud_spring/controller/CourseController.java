fdf7147a8ebceaaf60edaccb038e3179025791dd
Erick Rick <erickcredjes@gmail.com>
2024-09-22 23:51:32 -0300

    Implementando atualização para curso

--- src/main/java/com/erickcred/crud_spring/controller/CourseController.java
+++ src/main/java/com/erickcred/crud_spring/controller/CourseController.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,9 +33,10 @@ public class CourseController {
   }
 
   @GetMapping(path = "/{id}")
-  public Course findById(@PathVariable Long id) {
-    Course course = this.iCourseRepository.findById(id).get();
-    return course;
+  public ResponseEntity<Course> findById(@PathVariable Long id) {
+    return this.iCourseRepository.findById(id)
+      .map(course -> ResponseEntity.ok().body(course))
+      .orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping
@@ -46,4 +46,23 @@ public class CourseController {
     return course;
   }
 
+  @PutMapping(path = "/{id}")
+  public Course update(@PathVariable Long id, @RequestBody Course request) {
+    Course course = this.iCourseRepository.findById(id).get();
+    if (course != null) {
+      course = request;
+      this.iCourseRepository.save(course);
+    }
+    return course;
+  }
+
+  @DeleteMapping(path = "{id}")
+  public ResponseEntity<Course> delete(@PathVariable Long id) {
+    Course response = this.iCourseRepository.findById(id).get();
+
+    if (response != null)
+      this.iCourseRepository.delete(response);
+    
+    return ResponseEntity.ok(response);
+  }
 }
